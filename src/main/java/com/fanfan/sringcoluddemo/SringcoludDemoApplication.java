0251061d1cb54059387b7b9de210b1331c428571
fanfan1122 <1269495712@qq.com>
2018-07-05 18:50:22 +0800

    Initial commit

---
+++ src/main/java/com/fanfan/sringcoluddemo/SringcoludDemoApplication.java
@@ -0,0 +1,12 @@
+package com.fanfan.sringcoluddemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SringcoludDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SringcoludDemoApplication.class, args);
+	}
+}
---
+++ src/test/java/com/fanfan/sringcoluddemo/SringcoludDemoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.fanfan.sringcoluddemo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SringcoludDemoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
